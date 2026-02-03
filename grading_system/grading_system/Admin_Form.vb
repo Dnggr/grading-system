564826e56bff9406f9e5dcac0c825601f1ecc519
John Jewelry A. Dela Cruz <johnjewelrydelacruz@gmail.com>
2026-02-03 17:41:06 +0800

    added popupform for section creation

--- grading_system/grading_system/Admin_Form.vb
+++ grading_system/grading_system/Admin_Form.vb
@@ -4,8 +4,9 @@ Imports System.Windows.Forms
 Public Class Admin_Form
 
     Private response As DialogResult
+    Private toolStripState As String = ""
 
-    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
         Dim response As DialogResult
         response = MessageBox.Show("Are you sure you want to exit the program?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
 
@@ -14,4 +15,28 @@ Public Class Admin_Form
             Application.Exit() ' Ends the whole program
         End If
     End Sub
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        toolStripState = "section"
+    End Sub
+
+#Region "Loaders"
+    Private Sub LoadSections()
+        Try
+
+            Dim query As String = "select * from "
+            'Dim adapter As New OdbcDataAdapter(query, )
+            Dim dt As New DataTable
+
+            'adapter.Fill(dt)
+
+            DataGridView1.DataSource = dt
+            
+
+        Catch ex As Exception
+            MessageBox.Show("Error:" & ex.ToString, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+#End Region
 End Class
